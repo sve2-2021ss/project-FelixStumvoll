fb9e9bc30ea464ed1048918ce5753802ceaeaef1
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-05-29 23:03:41 +0200

    fix search query of users service

--- users/src/main/kotlin/com/gamelib/users/dal/repositories/UserRepository.kt
+++ users/src/main/kotlin/com/gamelib/users/dal/repositories/UserRepository.kt
@@ -3,12 +3,13 @@ package com.gamelib.users.dal.repositories
 import com.gamelib.users.dal.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
+    @Query("select u from User u where lower(u.name) like lower(concat('%',:name,'%'))")
     fun getAllByNameContaining(name: String): List<User>
+
     @Query("select u.friends from User u where u.id = :id")
     fun getFriendsOfUser(id: Long): List<User>
 }
