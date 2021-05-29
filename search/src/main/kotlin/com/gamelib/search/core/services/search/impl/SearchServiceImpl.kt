eb98382ac06ebc5b8bc3c6aef8877c77ad1ef76c
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-05-29 19:22:22 +0200

    enable error print in search service

--- search/src/main/kotlin/com/gamelib/search/core/services/search/impl/SearchServiceImpl.kt
+++ search/src/main/kotlin/com/gamelib/search/core/services/search/impl/SearchServiceImpl.kt
@@ -3,7 +3,6 @@ package com.gamelib.search.core.services.search.impl
 import com.gamelib.search.SearchConfig
 import com.gamelib.search.core.services.search.SearchService
 import com.gamelib.search.core.services.search.entities.SearchResult
-import com.gamelib.search.util.suppressError
 import io.github.resilience4j.circuitbreaker.CircuitBreaker
 import io.github.resilience4j.kotlin.circuitbreaker.CircuitBreakerConfig
 import io.github.resilience4j.kotlin.circuitbreaker.circuitBreaker
@@ -12,6 +11,7 @@ import io.github.resilience4j.kotlin.retry.retry
 import io.github.resilience4j.retry.Retry
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.merge
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
@@ -54,6 +54,8 @@ class SearchServiceImpl(private val config: SearchConfig) : SearchService {
             .bodyToFlow<SearchResult>()
             .circuitBreaker(circuitBreaker)
             .retry(retry)
-            .suppressError()
+            .catch {
+                println(it)
+            }
     }
 }
